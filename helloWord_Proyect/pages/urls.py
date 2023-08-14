aab02782ca66ae6af47bf6f571681e0c6da74e35
Yhilmar <yachaverrc@eafit.edu.co>
2023-08-14 14:53:51 -0500

    Terminando todas las actividades pendientes

--- helloWord_Proyect/pages/urls.py
+++ helloWord_Proyect/pages/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path 
 
-from .views import HomePageView 
+from .views import HomePageView, AboutPageView, ProductIndexView, ProductShowView, ProductCreateView
 
 urlpatterns = [ 
-    path("", HomePageView.as_view(), name='home') 
+    path("", HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('products/', ProductIndexView.as_view(), name='products'), 
+    path('products/create', ProductCreateView.as_view(), name='form'),
+    path('products/<str:id>', ProductShowView.as_view(), name='show'),
 ] 
--- helloWord_Proyect/pages/views.py
+++ helloWord_Proyect/pages/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render # here by default 
+from django.shortcuts import render, redirect
+
+from django import forms
 
 from django.http import HttpResponse # new 
 
 from django.views.generic import TemplateView 
+
+from django.views import View 
 
 # Create your views here. 
 
@@ -13,3 +17,97 @@ def homePageView(request): # new
 class HomePageView(TemplateView): 
 
     template_name = "pages/home.html"
+
+class AboutPageView(TemplateView):
+  template_name = 'pages/about.html'
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context.update({
+        "title": "About us - Online Store",
+        "subtitle": "About us",
+        "description": "This is an about page ...",
+        "author": "Developed by: Yhilmar Chaverra",
+    })
+
+    return context
+
+class Product: 
+   products = [
+      {"id":"1", "name":"TV", "description":"Best TV", "price": 150},
+      {"id":"2", "name":"iPhone", "description":"Best iPhone", "price": 300},
+      {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price": 500},
+      {"id":"4", "name":"Glasses", "description":"Best Glasses", "price": 200},
+  ]
+
+class ProductIndexView(View): 
+
+    template_name = 'products/index.html' 
+
+    def get(self, request): 
+        viewData = {} 
+        viewData["title"] = "Products - Online Store" 
+        viewData["subtitle"] =  "List of products" 
+        viewData["products"] = Product.products 
+
+        return render(request, self.template_name, viewData) 
+
+
+class ProductShowView(View):
+
+  template_name = 'products/show.html'
+
+  def get(self, request, id):
+    viewData = {}
+  
+    try:
+        product_id = int(id)
+        product = next((p for p in Product.products if int(p['id']) == product_id), None)
+
+        if product is None:
+            return redirect("home")  # Redirect to the home page if product is not found
+        
+        viewData["title"] = product["name"] + " - Online Store"
+        viewData["subtitle"] = product["name"] + " - Product information"
+        viewData["product"] = product
+        viewData["price"] = product["price"]
+
+        return render(request, self.template_name, viewData)
+    except ValueError:
+        return redirect("home")
+
+
+class ProductForm(forms.Form):
+  name = forms.CharField(required=True)
+  price = forms.FloatField(required=True)
+
+  def clean_price(self):
+    price = self.cleaned_data['price']
+    if price <= 0:
+        raise forms.ValidationError("The price must be greater than 0")
+    return price
+  
+
+class ProductCreateView(View):
+
+  template_name = 'products/create.html'
+
+  def get(self, request):
+      form = ProductForm()
+      viewData = {}
+      viewData["title"] = "Create product"
+      viewData["form"] = form
+      return render(request, self.template_name, viewData)
+
+  def post(self, request):
+      form = ProductForm(request.POST)
+      if form.is_valid():
+          success_message = "Product created"
+          form = ProductForm()  
+          viewData = {"title": "Create product", "form": form, "success_message": success_message}
+          return render(request, self.template_name, viewData)
+      else:
+          viewData = {}
+          viewData["title"] = "Create product"
+          viewData["form"] = form
+          return render(request, self.template_name, viewData)

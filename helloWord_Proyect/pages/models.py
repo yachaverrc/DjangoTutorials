ab50925c49b043cdee5b2df0ab7a3df6cc4346d2
Yhilmar <yachaverrc@eafit.edu.co>
2023-08-28 09:46:17 -0500

    subiendo el tutorial 3

--- helloWord_Proyect/pages/models.py
+++ helloWord_Proyect/pages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model): 
+
+    name = models.CharField(max_length=255) 
+
+    price = models.IntegerField() 
+
+    created_at = models.DateTimeField(auto_now_add=True) 
+
+    updated_at = models.DateTimeField(auto_now=True) 
--- helloWord_Proyect/pages/urls.py
+++ helloWord_Proyect/pages/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path 
 
-from .views import HomePageView, AboutPageView, ProductIndexView, ProductShowView, ProductCreateView
+from .views import (
+    HomePageView, AboutPageView, ProductIndexView, ProductShowView,
+    ProductCreateView, CartRemoveAllView, CartView, ImageViewFactory, ImageViewNoDI
+)
+
+from .utils import ImageLocalStorage
 
 urlpatterns = [ 
     path("", HomePageView.as_view(), name='home'),
@@ -8,4 +13,17 @@ urlpatterns = [
     path('products/', ProductIndexView.as_view(), name='products'), 
     path('products/create', ProductCreateView.as_view(), name='form'),
     path('products/<str:id>', ProductShowView.as_view(), name='show'),
+
+
+    path('cart/', CartView.as_view(), name='cart_index'), 
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'), 
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'), 
+    path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'), 
+
+    path('imagenotdi/', ImageViewNoDI.as_view(), name='imagenodi_index'),
+    path('image/save', ImageViewNoDI.as_view(), name='imagenodi_save'),
+
+
 ] 
